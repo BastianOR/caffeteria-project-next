1f3982e3918515856534de19bd005853370b7c6a
BastianOR <bastianossandon1@gmail.com>
2024-08-05 17:15:05 -0400

    Progressing with the landing page, specifically, the hero section.

--- src/components/landing-components/hero/hero.jsx
+++ src/components/landing-components/hero/hero.jsx
@@ -2,6 +2,14 @@ import "./hero.css";
 
 export default function Hero() {
   return (
-    <div className="Hero">Hero</div>
+    <div className="Hero">
+      <div className="landing-img">
+        <main>
+          <h1>Disfruta un café</h1>
+          <p>hecho con cariño</p>
+          <button>Mira nuestra carta</button>
+        </main>
+      </div>
+    </div>
   )
 }
